573b3c506f5f938e7000d9d08c18fe22c5b6c67b
yasintuncerr <yasintuncerr@gmail.com>
2025-07-16 00:17:53 +0300

    Add server initialization with image upload endpoint

---
+++ internal/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/histopathai/image-processing-service/config"
+	"github.com/histopathai/image-processing-service/internal/handler"
+)
+
+func Start(cfg *config.Config, h *handler.Handler) {
+	router := gin.Default()
+
+	router.POST("/upload", h.UploadImages)
+
+	router.Run(fmt.Sprintf(":%d", cfg.ServerConfig.Port))
+}
