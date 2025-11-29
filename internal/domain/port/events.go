8f2a8b44bbd68e842f065b70a6d5eb4d8ac9b703
yasintuncer <yasintuncerr@outlook.com>
2025-11-30 02:52:14 +0300

    Add EventHandler type definition to events package

--- internal/domain/port/events.go
+++ internal/domain/port/events.go
@@ -6,3 +6,5 @@ import "context"
 type Publisher interface {
 	Publish(ctx context.Context, topic string, data []byte, attributes map[string]string) error
 }
+
+type EventHandler func(ctx context.Context, data []byte, attributes map[string]string) error
