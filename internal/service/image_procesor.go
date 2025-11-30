1a26fe5df1db07329e4b631e6670d0459c9b42c0
yasintuncer <yasintuncerr@outlook.com>
2025-11-30 04:51:58 +0300

    Refactor ImageProcessingService to enhance file processing and integrate ImageInfoProcessor

--- internal/service/image_procesor.go
+++ internal/service/image_procesor.go
@@ -11,10 +11,11 @@ import (
 )
 
 type ImageProcessingService struct {
-	logger         *slog.Logger
-	dcrawProcessor *processors.DcrawProcessor
-	vipsProcessor  *processors.VipsProcessor
-	config         *config.Config
+	logger            *slog.Logger
+	dcrawProcessor    *processors.DcrawProcessor
+	vipsProcessor     *processors.VipsProcessor
+	fileInfoProcessor *processors.ImageInfoProcessor
+	config            *config.Config
 }
 
 func NewImageProcessingService(
@@ -22,10 +23,11 @@ func NewImageProcessingService(
 	cfg *config.Config,
 ) *ImageProcessingService {
 	return &ImageProcessingService{
-		logger:         logger,
-		dcrawProcessor: processors.NewDcrawProcessor(logger),
-		vipsProcessor:  processors.NewVipsProcessor(logger),
-		config:         cfg,
+		logger:            logger,
+		dcrawProcessor:    processors.NewDcrawProcessor(logger),
+		vipsProcessor:     processors.NewVipsProcessor(logger),
+		fileInfoProcessor: processors.NewImageInfoProcessor(logger),
+		config:            cfg,
 	}
 }
 
@@ -36,41 +38,44 @@ func (s *ImageProcessingService) ProcessFile(ctx context.Context, file *model.Fi
 			WithContext("fileID", file.ID)
 	}
 
-	processSuccess := false
-
-	defer func() {
-		if !processSuccess {
-			if err := workspace.Remove(); err != nil {
-				s.logger.Warn("Failed to clean up workspace on error",
-					"fileID", file.ID,
-					"error", err)
-			}
-		}
-	}()
+	wasDNGFile := s.isDNGFile(file)
+	tiffFilename := ""
 
 	if err := s.GetImageInfo(ctx, file); err != nil {
 		return nil, err
 	}
 
-	if s.isDNGFile(file) {
-		err := s.ConvertDNGToTIFF(ctx, workspace)
+	if wasDNGFile {
+		tiffFilename, err = s.ConvertDNGToTIFF(ctx, file, workspace)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	if err := s.GenerateThumbnail(ctx, workspace); err != nil {
+	if err := s.GenerateThumbnail(ctx, file, workspace); err != nil {
 		return nil, err
 	}
 
-	if err := s.GenerateDZI(ctx, workspace); err != nil {
+	if err := s.GenerateDZI(ctx, file, workspace); err != nil {
 		return nil, err
 	}
 
 	s.logger.Info("File processing workflow completed successfully",
 		"fileID", file.ID)
 
-	processSuccess = true
+	if wasDNGFile && tiffFilename != "" {
+		tiffPath := workspace.Join(tiffFilename)
+		if err := workspace.RemoveFile(tiffPath); err != nil {
+			s.logger.Warn("Failed to remove converted TIFF file from workspace",
+				"fileID", file.ID,
+				"tiffPath", tiffPath,
+				"error", err)
+		} else {
+			s.logger.Info("Removed converted TIFF file from workspace",
+				"fileID", file.ID,
+				"tiffPath", tiffPath)
+		}
+	}
 	return workspace, nil
 }
 
@@ -80,13 +85,13 @@ func (s *ImageProcessingService) GetImageInfo(ctx context.Context, file *model.F
 		"filename", file.Filename)
 
 	inputFilePath := file.AbsolutePath()
-	image_info, err := s.vipsProcessor.GetImageInfo(ctx, inputFilePath)
+	imageInfo, err := s.fileInfoProcessor.GetImageInfo(ctx, inputFilePath)
 
 	if err != nil {
 		return err
 	}
 
-	file.SetDimensions(image_info.Width, image_info.Height, image_info.Size)
+	file.SetDimensions(imageInfo.Width, imageInfo.Height, imageInfo.Size)
 	return nil
 }
 
@@ -95,13 +100,14 @@ func (s *ImageProcessingService) isDNGFile(file *model.File) bool {
 	return ext == ".dng"
 }
 
-func (s *ImageProcessingService) ConvertDNGToTIFF(ctx context.Context, workspace *model.Workspace) error {
+func (s *ImageProcessingService) ConvertDNGToTIFF(ctx context.Context, file *model.File, workspace *model.Workspace) (string, error) {
 	s.logger.Info("Converting DNG to TIFF",
-		"fileID", workspace.File().ID,
-		"filename", workspace.File().Filename)
+		"fileID", file.ID,
+		"filename", file.Filename)
 
-	inputFilePath := workspace.File().AbsolutePath()
-	outputFilePath := workspace.Join(workspace.File().BaseName() + ".tiff")
+	inputFilePath := file.AbsolutePath()
+	tiffFilename := file.BaseName() + ".tiff"
+	outputFilePath := workspace.Join(tiffFilename)
 
 	result, err := s.dcrawProcessor.DNGToTIFF(ctx, inputFilePath, outputFilePath, s.config.ImageProcessTimeoutMinute.FormatConversion)
 	if err != nil {
@@ -112,30 +118,35 @@ func (s *ImageProcessingService) ConvertDNGToTIFF(ctx context.Context, workspace
 			stderr = result.Stderr
 		}
 		s.logger.Error("DNG to TIFF conversion failed",
-			"fileID", workspace.File().ID,
+			"fileID", file.ID,
 			"stdout", stdout,
 			"stderr", stderr,
 			"error", err)
-		return err
+		return "", err
 	}
 
-	workspace.File().SetFilename(workspace.File().BaseName() + ".tiff")
-	workspace.File().SetFormat("tiff")
-	workspace.File().SetDir(workspace.Dir())
-
 	s.logger.Info("DNG to TIFF conversion succeeded",
-		"fileID", workspace.File().ID,
+		"fileID", file.ID,
 		"outputFile", outputFilePath)
 
-	return nil
+	return tiffFilename, nil
 }
 
-func (s *ImageProcessingService) GenerateThumbnail(ctx context.Context, workspace *model.Workspace) error {
+func (s *ImageProcessingService) GenerateThumbnail(ctx context.Context, file *model.File, workspace *model.Workspace) error {
 	s.logger.Info("Generating thumbnail",
-		"fileID", workspace.File().ID,
-		"filename", workspace.File().Filename)
+		"fileID", file.ID,
+		"filename", file.Filename)
 
-	inputFilePath := workspace.File().AbsolutePath()
+	var inputFilePath string
+
+	// DNG ise workspace'teki TIFF'i kullan, değilse orijinal dosyayı kullan
+	if s.isDNGFile(file) {
+		tiffFilename := file.BaseName() + ".tiff"
+		inputFilePath = workspace.Join(tiffFilename)
+	} else {
+		inputFilePath = file.AbsolutePath()
+	}
+
 	outputFilePath := workspace.Join("thumbnail.jpg")
 
 	result, err := s.vipsProcessor.CreateThumbnail(ctx, inputFilePath, outputFilePath,
@@ -151,7 +162,7 @@ func (s *ImageProcessingService) GenerateThumbnail(ctx context.Context, workspac
 			stderr = result.Stderr
 		}
 		s.logger.Error("Thumbnail generation failed",
-			"fileID", workspace.File().ID,
+			"fileID", file.ID,
 			"stdout", stdout,
 			"stderr", stderr,
 			"error", err)
@@ -159,18 +170,26 @@ func (s *ImageProcessingService) GenerateThumbnail(ctx context.Context, workspac
 	}
 
 	s.logger.Info("Thumbnail generation succeeded",
-		"fileID", workspace.File().ID,
+		"fileID", file.ID,
 		"outputFile", outputFilePath)
 
 	return nil
 }
 
-func (s *ImageProcessingService) GenerateDZI(ctx context.Context, workspace *model.Workspace) error {
+func (s *ImageProcessingService) GenerateDZI(ctx context.Context, file *model.File, workspace *model.Workspace) error {
 	s.logger.Info("Generating DZI",
-		"fileID", workspace.File().ID,
-		"filename", workspace.File().Filename)
+		"fileID", file.ID,
+		"filename", file.Filename)
 
-	inputFilePath := workspace.File().AbsolutePath()
+	var inputFilePath string
+
+	if s.isDNGFile(file) {
+		tiffFilename := file.BaseName() + ".tiff"
+		inputFilePath = workspace.Join(tiffFilename)
+	} else {
+		inputFilePath = file.AbsolutePath()
+	}
+
 	outputBase := workspace.Join("image")
 
 	result, err := s.vipsProcessor.CreateDZI(ctx,
@@ -187,7 +206,7 @@ func (s *ImageProcessingService) GenerateDZI(ctx context.Context, workspace *mod
 			stderr = result.Stderr
 		}
 		s.logger.Error("DZI generation failed",
-			"fileID", workspace.File().ID,
+			"fileID", file.ID,
 			"stdout", stdout,
 			"stderr", stderr,
 			"error", err)
@@ -195,7 +214,7 @@ func (s *ImageProcessingService) GenerateDZI(ctx context.Context, workspace *mod
 	}
 
 	s.logger.Info("DZI generation succeeded",
-		"fileID", workspace.File().ID,
+		"fileID", file.ID,
 		"outputBase", outputBase)
 
 	return nil
