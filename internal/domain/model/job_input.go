a311a32d8057833d56fb93e526c4fa6979e47ee8
yasintuncer <yasintuncerr@outlook.com>
2025-12-14 14:46:54 +0300

    refactor: Remove BucketName from JobInput and simplify NewJobInputFromEnv function

--- internal/domain/model/job_input.go
+++ internal/domain/model/job_input.go
@@ -5,23 +5,17 @@ import "fmt"
 type JobInput struct {
 	ImageID    string
 	OriginPath string
-	BucketName string
 }
 
-func NewJobInputFromEnv(imageID, originPath, bucketName string) (*JobInput, error) {
+func NewJobInputFromEnv(imageID, originPath string) (*JobInput, error) {
 	if imageID == "" {
 		return nil, fmt.Errorf("image ID is required")
 	}
 	if originPath == "" {
 		return nil, fmt.Errorf("origin path is required")
 	}
-	if bucketName == "" {
-		return nil, fmt.Errorf("bucket name is required")
-	}
-
 	return &JobInput{
 		ImageID:    imageID,
 		OriginPath: originPath,
-		BucketName: bucketName,
 	}, nil
 }
